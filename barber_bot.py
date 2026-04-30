5db981f341f16528c1c6ce55920a344a5eba54b6
jafrashit-max <jafrashit@gmail.com>
2026-04-30 11:54:28 +0500

    Update barber_bot.py
    fix

--- barber_bot.py
+++ barber_bot.py
@@ -1,5 +1,4 @@
 import asyncio
-import logging
 from datetime import datetime
 from telegram import Update, InlineKeyboardButton, InlineKeyboardMarkup
 from telegram.ext import Application, CommandHandler, CallbackQueryHandler, ContextTypes
